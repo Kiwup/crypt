8323150353aba518019ed56a88368adc1e6157a9
Gauthier Deroo <g.deroo@gmail.com>
2018-01-16 14:21:45 +0100

    chore Update tests to use password like > key+password

--- tests/crypt.test.js
+++ tests/crypt.test.js
@@ -9,15 +9,15 @@ const KEY = '__MY_EXAMPLE_KEY__'
 const STRING_TO_ENCRYPT = 'coco l escargot'
 
 test('Encrypt String', (assert) => {
-  const encrypted = encrypt(STRING_TO_ENCRYPT, `${SECRET}${KEY}`)
+  const encrypted = encrypt(STRING_TO_ENCRYPT, `${KEY}${SECRET}`)
 
-  assert.equal(encrypted, 'Y2YzYTg3YzEwZGNhODA2MzgxNTIwMzcyYTdmMTkzMDI=')
+  assert.equal(encrypted, 'NjdjY2ZjNTBjNDcyY2FhM2JhZmYwZWE2YTZhZDE3ZjI=')
   assert.end()
 })
 
 test('Encrypt and Decrypt String', (assert) => {
-  const encrypted = encrypt(STRING_TO_ENCRYPT, `${SECRET}${KEY}`)
-  const decrypted = decrypt(encrypted, `${SECRET}${KEY}`)
+  const encrypted = encrypt(STRING_TO_ENCRYPT, `${KEY}${SECRET}`)
+  const decrypted = decrypt(encrypted, `${KEY}${SECRET}`)
 
   assert.equal(decrypted, STRING_TO_ENCRYPT)
   assert.end()
